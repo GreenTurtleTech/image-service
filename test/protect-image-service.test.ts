7af2ff71be9f9b45ef93e15f3c1c78daf16196ec
Josh <jpollock412@gmail.com>
2023-08-18 17:55:41 -0400

    Initial commit

---
+++ test/protect-image-service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ProtectImageService from '../lib/protect-image-service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/protect-image-service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ProtectImageService.ProtectImageServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
