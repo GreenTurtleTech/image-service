6926e08f4bbc8f398e7c50cbb1e5534b334dd88e
Josh <jpollock412@gmail.com>
2023-11-07 17:53:00 -0500

    #2 no hardcoded defaults in env also .env.example

--- bin/image-service.ts
+++ bin/image-service.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-import 'dotenv/config'
+import 'dotenv/config';
 
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import 'source-map-support/register';
 import { ImageServiceStack } from '../lib/image-service-stack';
 
 const env =  {
@@ -10,16 +10,16 @@ const env =  {
   region: process.env.AWS_REGION || 'eu-west-2' as string,
 };
 const app = new cdk.App();
-new ImageServiceStack(app, 'ImageServiceStack', {
+new ImageServiceStack(app, 'ImageService', {
   env,
-  bucketName: process.env.BUCKET_NAME as string ||'protect-earth-prod-photos-58374',
+  bucketName: process.env.BUCKET_NAME as string,
   errorsEmail: process.env.ERRORS_EMAIL as string,
-  treeApiUrl: process.env.TREE_API_URL as string || 'https://quaint-dublin-zvwqq2sxihei.vapor-farm-b1.com/api'
+  treeApiUrl: process.env.TREE_API_URL as string,
 });
 
-new ImageServiceStack(app, 'ImageServiceStackStaging', {
+new ImageServiceStack(app, 'ImageService_Staging', {
   env,
-  bucketName: process.env.BUCKET_NAME_STAGING as string ||'protect-earth-stag-photos-57324',
+  bucketName: process.env.BUCKET_NAME_STAGING as string,
   errorsEmail: process.env.ERRORS_EMAIL as string,
-  treeApiUrl: process.env.TREE_API_URL as string || 'https://royal-delhi-n3vjzgjonhc9.vapor-farm-f1.com/api'
+  treeApiUrl: process.env.TREE_API_URL_STAGING as string || process.env.TREE_API_URL as string
 });
